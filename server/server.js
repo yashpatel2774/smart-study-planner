b74cee5b164913b4261454be03de4a12fa12fee5
yashpatel2774 <yp502858@gmail.com>
2026-02-25 15:37:50 +0530

    fix cors preflight

--- server/server.js
+++ server/server.js
@@ -15,15 +15,14 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed"));
-    }
-  },
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
+// VERY IMPORTANT (this fixes login/register)
+app.options("*", cors());
 
 app.use(express.json());
 

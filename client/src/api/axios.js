58540c74e92ee5a65378962e5fbe3dc5e083d440
yashpatel2774 <yp502858@gmail.com>
2026-02-26 12:29:42 +0530

    Final axios changes made

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 
-// ⚠️ DO NOT USE import.meta.env for now
-// Hardcode the real Render backend URL
-
 const API = axios.create({
-  baseURL: "https://smart-study-backend-w1pr.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: false
 });
 
-// attach token automatically
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem("user"));
 
